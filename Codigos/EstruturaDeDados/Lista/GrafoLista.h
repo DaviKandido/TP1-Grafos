8e5231e4fd1f6c6476b5f6f252ce68c1267eebef
VitorSetragni <vitorsetragni@gmail.com>
2025-08-29 11:03:58 -0300

    Algumas funções simples para Lista

--- Codigos/EstruturaDeDados/Lista/GrafoLista.h
+++ Codigos/EstruturaDeDados/Lista/GrafoLista.h
@@ -22,5 +22,21 @@ class GrafoLista : public IGrafo<int> {
       bool direcionado;
       vector<Vertice> arr;
 
-      //a
+      bool verticeValido(int v) const {
+        return (0 <= v && v < numVertices);
+      }
+
+      int getQuantidadeVertices() const override {
+         return numVertices;
+      }
+
+      int getQuantidadeArestas() const override {
+        return numArestas;
+      }
+
+      bool existeVertice(int v) const override {
+        return verticeValido(v);
+      }
+
+      
 };
