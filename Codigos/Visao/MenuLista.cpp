5b4de5b73fc7ae6ba4b6042d38c75332794bc1c0
VitorSetragni <vitorsetragni@gmail.com>
2025-08-29 11:52:40 -0300

    Modificação MenuLista

--- Codigos/Visao/MenuLista.cpp
+++ Codigos/Visao/MenuLista.cpp
@@ -5,30 +5,29 @@
 using namespace std;
 
 void menu() {
-    int opcao = 0;
-    int continuar = 1;
-    cout << "> Menu > Lista" << endl << endl;
-    cout << "1) Instanciar Grafo" << endl;
-    cout << "2) Adicionar Vértice" << endl;
-    cout << "3) Adicionar Aresta" << endl;
-    cout << "4) Remover Vértice" << endl;
-    cout << "5) Remover Aresta" << endl;
-    cout << "6) Consultar Vasinhos de um Vértice" << endl;
-    cout << "7) Consultar Sucessores de um Vértice - DFS" << endl;
-    cout << "8) Consultar Predecessores de um Vértice - DFS" << endl;
-    cout << "9) Remover Grafo" << endl;
-    cout << "10) Imprimir Grafo" << endl;
-    cout << "0) - Sair" << endl << endl;
+    int opcao = -1;
+    while (opcao != 0) {
+        cout << "> Menu > Lista" << endl << endl;
+        cout << "1) Instanciar Grafo" << endl;
+        cout << "2) Adicionar Vértice" << endl;
+        cout << "3) Adicionar Aresta" << endl;
+        cout << "4) Remover Vértice" << endl;
+        cout << "5) Remover Aresta" << endl;
+        cout << "6) Consultar Vasinhos de um Vértice" << endl;
+        cout << "7) Consultar Sucessores de um Vértice - DFS" << endl;
+        cout << "8) Consultar Predecessores de um Vértice - DFS" << endl;
+        cout << "9) Remover Grafo" << endl;
+        cout << "10) Imprimir Grafo" << endl;
+        cout << "0) - Sair" << endl << endl;
 
-    cout << "Opção: ";
-    cin >> opcao;
+        cout << "Opção: ";
+        cin >> opcao;
 
     /**
      * @attention Acredito que um do while seja uma forma melhor de trabalhar a repetição, veja
      * menuMatriz
      *
      */
-    while (continuar == 1) {
         switch (opcao) {
             case 1:
                 /* code */
@@ -69,16 +68,14 @@ void menu() {
             case 10:
                 /* code */
                 break;
-
+            case 0:
+                cout << "Obrigado pela preferencia!";
+                break;
             default:  // Vai funcionar para parar a função
                 cout << "Opção inválida!" << endl;
                 break;
         }
 
-        cout << "Desejar continuar a fazer operacoes?" << endl;
-        cout << "1 - SIM" << endl;
-        cout << "2 - NAO" << endl;
-        cout << "opcao : ";
-        cin >> continuar;
+       
     }
 }
