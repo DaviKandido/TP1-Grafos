8969fc0feff6fd87371ff5105da7d6ad935927ad
VitorSetragni <vitorsetragni@gmail.com>
2025-09-03 15:12:10 -0300

    MenuLista com algumas funçoes

--- Codigos/EstruturaDeDados/Lista/GrafoLista.cpp
+++ Codigos/EstruturaDeDados/Lista/GrafoLista.cpp
@@ -11,32 +11,50 @@ class GrafoLista : public IGrafo<int> {
 
    public:
 
-      struct Vertice
-      {
-         int peso;
-         string rotulo;
-         Vertice* next;
-      };
-      int numVertices;
-      int numArestas;
-      bool direcionado;
-      vector<Vertice> arr;
+    struct Vertice{
+      int peso;
+      string rotulo;
+      Vertice* next;
+    };
+    int numVertices;
+    int numArestas;
+    vector<Vertice> arr;
+    bool direcionado;
+    bool verticePonderado;
+    bool arestaPonderada;
+    bool verticeRotulado;
+    bool arestaRotulada;
 
-      bool verticeValido(int v) const {
-        return (0 <= v && v < numVertices);
-      }
-
-      int getQuantidadeVertices() const override {
-         return numVertices;
-      }
-
-      int getQuantidadeArestas() const override {
-        return numArestas;
-      }
-
-      bool existeVertice(int v) const override {
-        return verticeValido(v);
-      }
-
+    GrafoLista(int totalVertices, bool direcionado, bool verticePonderado, bool arestaPonderada, bool verticeRotulado, bool arestaRotulada) {
       
+      // Atribuir valores iniciais
+      this->numVertices = totalVertices;
+      this->numArestas = 0;
+
+      // Atribuir variáveis de controle
+      this->direcionado = direcionado;
+      this->verticePonderado = verticePonderado;
+      this->arestaPonderada = arestaPonderada;
+      this->verticeRotulado = verticeRotulado;
+      this->arestaRotulada = arestaRotulada;
+
+    }
+
+    bool verticeValido(int v) const {
+      return (0 <= v && v < numVertices);
+    }
+
+    int getQuantidadeVertices() const override {
+       return numVertices;
+    }
+
+    int getQuantidadeArestas() const override {
+      return numArestas;
+    }
+
+    bool existeVertice(int v) const override {
+      return verticeValido(v);
+    }
+
+    
 };
--- Codigos/Visao/MenuLista.cpp
+++ Codigos/Visao/MenuLista.cpp
@@ -7,184 +7,189 @@
 using namespace std;
 #define maxVertice 2147483647
 
-
- /**
-     * Menu de operações para manipular o grafo.
-     *
-     * Pergunta ao usuario qual opção ele deseja fazer, dependendo da escolha,
-     * chama a respectiva função para realizar a operação que desejada.
-     *
-     * @see intanciaGrafo()
-     * @see adicionaVertice()
-     * @see adicionaAresta()
-     * @see removeVertice()
-     * @see removeAresta()
-     * @see consultaVizinhosVertice()
-     * @see consultarSucessoresVertice()
-     * @see consultarPredecessoresVertice()
-     * @see removeGrafo()
-     * @see imprimeGrafo()
-     */
-
-void menu() {
-    int opcao = -1;
-    while (opcao != 0) {
-        cout << "> Menu > Lista" << endl << endl;
-        cout << "1) Instanciar Grafo" << endl;
-        cout << "2) Adicionar Vértice" << endl;
-        cout << "3) Adicionar Aresta" << endl;
-        cout << "4) Remover Vértice" << endl;
-        cout << "5) Remover Aresta" << endl;
-        cout << "6) Consultar Vasinhos de um Vértice" << endl;
-        cout << "7) Consultar Sucessores de um Vértice - DFS" << endl;
-        cout << "8) Consultar Predecessores de um Vértice - DFS" << endl;
-        cout << "9) Remover Grafo" << endl;
-        cout << "10) Imprimir Grafo" << endl;
-        cout << "0) - Sair" << endl << endl;
-
-        cout << "Opção: ";
-        cin >> opcao;
-
-        switch (opcao) {
-            case 1:
-                intanciaGrafo();
-                break;
-
-            case 2:
-                adicionaVertice();
-                break;
-
-            case 3:
-                adicionaAresta();
-                break;
-
-            case 4:
-                removeVertice();
-                break;
-
-            case 5:
-                removeAresta();
-                break;
-
-            case 6:
-                consultaVizinhosVertice();
-                break;
-
-            case 7:
-                consultarSucessoresVertice();
-                break;
-
-            case 8:
-                consultarPredecessoresVertice();
-                break;
-
-            case 9:
-                removeGrafo();
-                break;
-
-            case 10:
-                imprimeGrafo();
-                break;
-            case 0:
-                cout << "Obrigado pela preferencia!";
-                break;
-            default: 
-                cout << "Opção inválida!" << endl;
-                cout << "Porfavor escolha uma das opções a baixo" << endl;
-                break;
-        }
-
-       
-    }
-}
+class MenuLista{
+    private:
+        GrafoLista* grafo;
 
     /**
-     * Cria um grafo e o armazena em 'grafo'. O grafo pode ser direcionado ou não-direcionado, ponderado ou não ponderado e rotulado ou não rotulado.
-     */
-    void intanciaGrafo() {
-        cout << "\n> Menu > Lista > Instanciar Grafo" << endl << endl;
+         * Menu de operações para manipular o grafo.
+         *
+         * Pergunta ao usuario qual opção ele deseja fazer, dependendo da escolha,
+         * chama a respectiva função para realizar a operação que desejada.
+         *
+         * @see intanciaGrafo()
+         * @see adicionaVertice()
+         * @see adicionaAresta()
+         * @see removeVertice()
+         * @see removeAresta()
+         * @see consultaVizinhosVertice()
+         * @see consultarSucessoresVertice()
+         * @see consultarPredecessoresVertice()
+         * @see removeGrafo()
+         * @see imprimeGrafo()
+         */
 
-        // Atributo do grafo
-        int numVertices;
+    void menu() {
+        int opcao = -1;
+        while (opcao != 0) {
+            cout << "> Menu > Lista" << endl << endl;
+            cout << "1) Instanciar Grafo" << endl;
+            cout << "2) Adicionar Vértice" << endl;
+            cout << "3) Adicionar Aresta" << endl;
+            cout << "4) Remover Vértice" << endl;
+            cout << "5) Remover Aresta" << endl;
+            cout << "6) Consultar Vasinhos de um Vértice" << endl;
+            cout << "7) Consultar Sucessores de um Vértice - DFS" << endl;
+            cout << "8) Consultar Predecessores de um Vértice - DFS" << endl;
+            cout << "9) Remover Grafo" << endl;
+            cout << "10) Imprimir Grafo" << endl;
+            cout << "0) - Sair" << endl << endl;
 
-        // Variáveis de controle
-        bool direcionado;
-        bool verticePonderado;
-        bool arestaPonderada;
-        bool verticeRotulado;
-        bool arestaRotulada;
+            cout << "Opção: ";
+            cin >> opcao;
 
-        // Ler as informações do usuário
-        numVertices = lerInteiro("Número de vértices: ", 1, maxVertice);
+            switch (opcao) {
+                case 1:
+                    intanciaGrafo();
+                    break;
 
-        direcionado      = lerBoolean("O grafo é direcionado?      (S/N): ");
-        verticePonderado = lerBoolean("Os vértices são ponderados? (S/N): ");
-        arestaPonderada  = lerBoolean("As arestas são ponderadas?  (S/N): ");
-        verticeRotulado  = lerBoolean("Os vértices são rotulados?  (S/N): ");
-        arestaRotulada   = lerBoolean("As arestas são rotuladas?   (S/N): ");
-        // Aqui ainda ta faltando implementar o GrafoLista
-}
+                case 2:
+                    adicionaVertice();
+                    break;
 
-void adicionaVertice(){
-    cout << "\n> Menu > Lista > Adicionar Vértice" << endl << endl;
+                case 3:
+                    adicionaAresta();
+                    break;
 
-    //Variaveis nas suas formas padrão caso seja não rotulado e não ponderado
-    string rotulo = "";
-    int peso = 1;
-    //teste de ponderado e de rotulo
-    if(grafo->verticeRotulado == 1){
-        rotulo = lerRotulo("Rótulo do vértice: ");
+                case 4:
+                    removeVertice();
+                    break;
+
+                case 5:
+                    removeAresta();
+                    break;
+
+                case 6:
+                    consultaVizinhosVertice();
+                    break;
+
+                case 7:
+                    consultarSucessoresVertice();
+                    break;
+
+                case 8:
+                    consultarPredecessoresVertice();
+                    break;
+
+                case 9:
+                    removeGrafo();
+                    break;
+
+                case 10:
+                    imprimeGrafo();
+                    break;
+                case 0:
+                    cout << "Obrigado pela preferencia!";
+                    break;
+                default: 
+                    cout << "Opção inválida!" << endl;
+                    cout << "Porfavor escolha uma das opções a baixo" << endl;
+                    break;
+            }
+
+        
+        }
     }
 
-    if(grafo->verticePonderado == 1){
-        rotulo = lerInteiro("Rótulo do vértice: ");
+        /**
+         * Cria um grafo e o armazena em 'grafo'. O grafo pode ser direcionado ou não-direcionado, ponderado ou não ponderado e rotulado ou não rotulado.
+         */
+        void intanciaGrafo() {
+            cout << "\n> Menu > Lista > Instanciar Grafo" << endl << endl;
+
+            // Atributo do grafo
+            int numVertices;
+
+            // Variáveis de controle
+            bool direcionado;
+            bool verticePonderado;
+            bool arestaPonderada;
+            bool verticeRotulado;
+            bool arestaRotulada;
+
+            // Ler as informações do usuário
+            numVertices = lerInteiro("Número de vértices: ", 1, maxVertice);
+
+            direcionado      = lerBoolean("O grafo é direcionado?      (S/N): ");
+            verticePonderado = lerBoolean("Os vértices são ponderados? (S/N): ");
+            arestaPonderada  = lerBoolean("As arestas são ponderadas?  (S/N): ");
+            verticeRotulado  = lerBoolean("Os vértices são rotulados?  (S/N): ");
+            arestaRotulada   = lerBoolean("As arestas são rotuladas?   (S/N): ");
+            // Aqui ainda ta faltando implementar o GrafoLista
+            this->grafo = new GrafoLista(numVertices, direcionado, verticePonderado, arestaPonderada, verticeRotulado, arestaRotulada);
     }
 
-    //Agora tem que chamar uma função de GrafoLista que ainda não foi feito
-}
+    void adicionaVertice(){
+        cout << "\n> Menu > Lista > Adicionar Vértice" << endl << endl;
 
-void adicionaAresta(){
-    cout << "\n> Menu > Lista > Adicionar Aresta" << endl << endl;
+        //Variaveis nas suas formas padrão caso seja não rotulado e não ponderado
+        string rotulo = "";
+        int peso = 1;
+        //teste de ponderado e de rotulo
+        if(grafo->verticeRotulado == 1){
+            rotulo = lerRotulo("Rótulo do vértice: ");
+        }
 
-    //Variaveis nas suas formas padrão caso seja não rotulado e não ponderado
-    string rotulo = "";
-    int peso = 1;
-    //teste de ponderado e de rotulo
-    if(grafo->arestaRotulado == 1){
-        rotulo = lerRotulo("Rótulo da aresta: ");
+        if(grafo->verticePonderado == 1){
+            rotulo = lerInteiro("Peso do vértice: ",(0 - maxVertice),maxVertice);
+        }
+
+        //Agora tem que chamar uma função de GrafoLista que ainda não foi feito
     }
 
-    if(grafo->arestaPonderado == 1){
-        rotulo = lerInteiro("Rótulo da aresta: ");
+    void adicionaAresta(){
+        cout << "\n> Menu > Lista > Adicionar Aresta" << endl << endl;
+
+        //Variaveis nas suas formas padrão caso seja não rotulado e não ponderado
+        string rotulo = "";
+        int peso = 1;
+        //teste de ponderado e de rotulo
+        if(grafo->arestaRotulada == 1){
+            rotulo = lerRotulo("Rótulo da aresta: ");
+        }
+
+        if(grafo->arestaPonderada == 1){
+            rotulo = lerInteiro("Peso da aresta: ",(0 - maxVertice),maxVertice);
+        }
+
+        //Agora tem que chamar uma função de GrafoLista que ainda não foi feito
     }
 
-    //Agora tem que chamar uma função de GrafoLista que ainda não foi feito
-}
+    void removeVertice(){
 
-void removeVertice(){
+    }
 
-}
+    void removeAresta(){
 
-void removeAresta(){
+    }
 
-}
+    void consultaVizinhosVertice(){
 
-void consultaVizinhosVertice(){
+    }
 
-}
+    void consultarSucessoresVertice(){
 
-void consultarSucessoresVertice(){
+    }
 
-}
+    void consultarPredecessoresVertice(){
 
-void consultarPredecessoresVertice(){
+    }
 
-}
+    void removeGrafo(){
 
-void removeGrafo(){
+    }
 
-}
+    void imprimeGrafo(){
 
-void imprimeGrafo(){
-
-}
+    }
+};
