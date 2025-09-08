e29308a12636fa29d15dc985190bce154b9d0f4d
VitorSetragni <vitorsetragni@gmail.com>
2025-09-07 21:33:16 -0300

    Menu MenuLista

--- Codigos/Visao/MenuLista.cpp
+++ Codigos/Visao/MenuLista.cpp
@@ -251,10 +251,49 @@ class MenuLista {
 
     void consultarSucessoresVertice() {
         cout << "\n> Menu > Lista > Consultar Fecho Transitivo Direto de Vértice" << endl << endl;
+           // Variavel para indicar a posição do vertice no array
+        int verticei;
+
+        grafo->imprimir();
+
+        //pegando a posição dos vertices na Lista
+        verticei = lerInteiro("Digite o número do vértice que deseja remover", 0,grafo->getQuantidadeVertices());
+
+        //O vertice esta dentro da listaPrincipal que é feita de NoVertice e dentro do Novertice que esta o vertice
+        Vertice inicioV = grafo->listaPrincipal[verticei].vertice;
+        
+        vector<Vertice> vizinho = grafo->fechoTransitivoDireto(inicioV);
+        if (vizinho.empty()) {
+            cout << "Sem vizinhos\n";
+        } else {
+            for (const Vertice& v : vizinho) {
+                cout << "ID=" << v.getId() << " | Peso=" << v.getPeso() << " | Rotulo=" << v.getRotulo() << '\n';
+            }
+        }
     }
 
     void consultarPredecessoresVertice() {
         cout << "\n> Menu > Lista > Consultar Fecho Transitivo Inverso de Vértice" << endl << endl;
+           // Variavel para indicar a posição do vertice no array
+        int verticei;
+
+        grafo->imprimir();
+
+        //pegando a posição dos vertices na Lista
+        verticei = lerInteiro("Digite o número do vértice que deseja remover", 0,grafo->getQuantidadeVertices());
+
+        //O vertice esta dentro da listaPrincipal que é feita de NoVertice e dentro do Novertice que esta o vertice
+        Vertice inicioV = grafo->listaPrincipal[verticei].vertice;
+        
+        vector<Vertice> vizinho = grafo->fechoTransitivoInverso(inicioV);
+        if (vizinho.empty()) {
+            cout << "Sem vizinhos\n";
+        } else {
+            for (const Vertice& v : vizinho) {
+                cout << "ID=" << v.getId() << " | Peso=" << v.getPeso() << " | Rotulo=" << v.getRotulo() << '\n';
+    
+            }
+        }
     }
 
     void removeGrafo() {
